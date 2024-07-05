0839ffc0624ccb044020f683bd1cd54e47ae392c
趙子賢 <danny900714@gmail.com>
2024-07-05 17:05:44 +0800

    Change package version to 0.1a1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="kalgory",
-    version="0.1.0-dev",
+    version="0.1a1",
     python_requires=">=3.10",
     author="kalgory",
     author_email="dev@kalgory.com",
