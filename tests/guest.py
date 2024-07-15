de7dc1307badd954e23d8779f8df224f1d00e652
Wei <chuweichang0319@gmail.com>
2024-07-15 17:07:26 +0800

    make it more complicated

--- tests/guest.py
+++ tests/guest.py
@@ -1,6 +1,6 @@
 from kalgory.component import BaseBlock
 from typing import Tuple
-
 class Block(BaseBlock):
-    def handle(key: str, y: int) -> Tuple[str, int]:
-        return "HEl123lo"
+    def handle(self, x:int, y:float, z:list[int, str]) -> Tuple[int, str]:
+        y = int(y)
+        return  (x + y, z)
