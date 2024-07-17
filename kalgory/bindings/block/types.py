7a30449b4a2fdf3b39c69cce3dad72487f551566
Wei <chuweichang0319@gmail.com>
2024-07-17 11:07:38 +0800

    add MPL licence

--- kalgory/bindings/block/types.py
+++ kalgory/bindings/block/types.py
@@ -1,3 +1,7 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 from typing import TypeVar, Generic, Union, Optional, Protocol, Tuple, List, Any
 from enum import Flag, Enum, auto
 from dataclasses import dataclass
