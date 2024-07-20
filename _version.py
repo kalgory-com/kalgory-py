ebc099677abdbe3c8f5ad03f4ff7b49dd7aa1c9f
Wei <chuweichang0319@gmail.com>
2024-07-21 00:23:44 +0800

    test

--- _version.py
+++ _version.py
@@ -12,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = '0.1.4rc1+g291bd09'
-__version_tuple__ = version_tuple = (0, 1, 4, 'g291bd09')
+__version__ = version = '0.1.4rc1+g51ab0a2.d20240720'
+__version_tuple__ = version_tuple = (0, 1, 4, 'g51ab0a2.d20240720')
