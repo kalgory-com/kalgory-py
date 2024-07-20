51ab0a26dd549588ea763a3e86586d7783fd8800
Wei <chuweichang0319@gmail.com>
2024-07-21 00:09:25 +0800

    fix version name

--- version_scheme.py
+++ version_scheme.py
@@ -14,6 +14,7 @@ def custom_local_scheme(version):
         pr_number = getenv("GITHUB_REF").split("/")[2]
         return f".pr{pr_number}"
     elif getenv("GITHUB_SHA"):
-        return getenv(".GITHUB_SHA")[:7]
+        sha_num = getenv("GITHUB_SHA")[:8]
+        return f".{sha_num}"
     else:
         return get_local_node_and_date(version)
