0e94048865b38965bbb997dade1f3a37a45afc0c
Wei <chuweichang0319@gmail.com>
2024-07-21 00:00:02 +0800

    test publish

--- _version.py
+++ _version.py
@@ -3,7 +3,6 @@
 TYPE_CHECKING = False
 if TYPE_CHECKING:
     from typing import Tuple, Union
-
     VERSION_TUPLE = Tuple[Union[int, str], ...]
 else:
     VERSION_TUPLE = object
@@ -13,5 +12,5 @@ __version__: str
 __version_tuple__: VERSION_TUPLE
 version_tuple: VERSION_TUPLE
 
-__version__ = version = "0.1.4rc2.dev0"
-__version_tuple__ = version_tuple = (0, 1, 4, "dev0")
+__version__ = version = '0.1.4rc1+g1beecfe.d20240720'
+__version_tuple__ = version_tuple = (0, 1, 4, 'g1beecfe.d20240720')
